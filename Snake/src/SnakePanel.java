55508935e95d92667c9eb70d6462913109ab6363
naum_tomov <n.tomov@student.utwente.nl>
2020-12-20 23:45:27 +0200

    Working version with no bugs

--- Snake/src/SnakePanel.java
+++ Snake/src/SnakePanel.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 public class SnakePanel extends JPanel implements ActionListener {
@@ -12,16 +14,18 @@ public class SnakePanel extends JPanel implements ActionListener {
     static final int HEIGHT = 720;
     static final int UNIT_SIZE = 20;
     int delay = 50;
-    final int x[] = new int[(WIDTH/UNIT_SIZE) + 1];
-    final int y[] = new int[(HEIGHT/UNIT_SIZE) + 1];
+    final int[] x = new int[(WIDTH*HEIGHT/UNIT_SIZE)];
+    final int[] y = new int[(HEIGHT*WIDTH/UNIT_SIZE)];
     int bodyParts = 6;
     int applesEaten = 0;
     int appleX;
     int appleY;
     char direction = 'R';
-    boolean running = false;
+    volatile boolean running = false;
     Random random;
     Timer timer;
+    Queue<Character> commandQueue = new LinkedList<>();
+
 
     /**
      * Constructor for the Snake Panel class.
@@ -50,8 +54,8 @@ public class SnakePanel extends JPanel implements ActionListener {
      * @ensures the apple will appear on the panel
      */
     public void newApple() {
-        appleX = random.nextInt((int)(WIDTH/UNIT_SIZE))*UNIT_SIZE;
-        appleY = random.nextInt((int)(HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+        appleX = random.nextInt(WIDTH/UNIT_SIZE)*UNIT_SIZE;
+        appleY = random.nextInt(HEIGHT/UNIT_SIZE)*UNIT_SIZE;
     }
 
     /**
@@ -70,26 +74,32 @@ public class SnakePanel extends JPanel implements ActionListener {
     public void draw(Graphics g) {
         if (running) {
             // score
-            g.setColor(Color.red);
+            g.setColor(Color.white);
             g.setFont( new Font("Helvetica",Font.BOLD, 20));
             FontMetrics metrics1 = getFontMetrics(g.getFont());
-            g.drawString("Score: "+applesEaten, (WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, UNIT_SIZE);
+            g.drawString("Score: "+applesEaten, (WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, UNIT_SIZE - 3);
             // draw apple
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
             // draw snake head
-            g.setColor(new Color(0, 90, 150));
-            g.fillRect(x[0], y[0], UNIT_SIZE, UNIT_SIZE);
+            g.setColor(Color.green);
+            g.fillOval(x[0], y[0], UNIT_SIZE, UNIT_SIZE);
 
             // draw snake body
-            g.setColor(new Color(50, 255, 185));
-            for (int i = bodyParts - 1; i > 0; i--) {
+            for (int i = bodyParts - 2; i > 0; i--) {
+                if (i%2 == 0) {
+                    g.setColor(new Color(173,255,47));
+                }
+                else {
+                    g.setColor(new Color(34,139,34));
+
+                }
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
             }
 
             // draw snake tail
-            g.fillArc(x[bodyParts], y[bodyParts], UNIT_SIZE, UNIT_SIZE, 120, 120);
+            g.fillArc(x[bodyParts-1] + UNIT_SIZE/5, y[bodyParts-1]  + UNIT_SIZE/5, UNIT_SIZE, UNIT_SIZE, 60, 120);
         }
         else {
             gameOver(g);
@@ -101,6 +111,39 @@ public class SnakePanel extends JPanel implements ActionListener {
      * @requires direction == 'R' || direction == 'L' || direction == 'U' || direction == 'D'
      */
     public void move() {
+        boolean set = false;
+        while (commandQueue.size() > 0) {
+            System.out.println(commandQueue);
+             switch (commandQueue.remove()) {
+                 case 'L':
+                     if (direction != 'R' && direction != 'L') {
+                         direction = 'L';
+                         set = true;
+                     }
+                     break;
+                 case 'R':
+                     if (direction != 'L' && direction != 'R') {
+                         direction = 'R';
+                         set = true;
+                     }
+                     break;
+                 case 'U':
+                     if (direction != 'D' && direction != 'U') {
+                         direction = 'U';
+                         set = true;
+                     }
+                     break;
+                 case 'D':
+                     if (direction != 'U' && direction != 'D') {
+                         direction = 'D';
+                         set = true;
+                     }
+                     break;
+             }
+             if (set) {
+                 break;
+             }
+        }
         // each body part goes to the next
         for (int bodyPart = bodyParts; bodyPart > 0; bodyPart--) {
             x[bodyPart] = x[bodyPart - 1];
@@ -108,48 +151,52 @@ public class SnakePanel extends JPanel implements ActionListener {
         }
         switch(direction) {
             case 'L':
-                x[0] = x[0] - UNIT_SIZE;
+                x[0] -= UNIT_SIZE;
                 break;
             case 'R':
-                x[0] = x[0] + UNIT_SIZE;
+                x[0] += UNIT_SIZE;
                 break;
             case 'U':
-                y[0] = y[0] - UNIT_SIZE;
+                y[0] -= UNIT_SIZE;
                 break;
             case 'D':
-                y[0] = y[0] + UNIT_SIZE;
+                y[0] += UNIT_SIZE;
                 break;
-
         }
+
     }
 
+    /**
+     * Checks if the head position is inside a wall or the body. Ends the game if you collide with body.
+     *   Loops around if you hit a wall.
+     */
     public void checkCollisions() {
         // check for body collision
         for (int i = bodyParts; i > 0; i--) {
             if (x[0] == x[i] && y[0] == y[i]) {
                 running = false;
+                break;
             }
         }
 
         // check for upper wall collision
         if (y[0] < 0) {
-            running = false;
+            y[0] = HEIGHT - UNIT_SIZE;
         }
         // check for lower wall collision
-        if (y[0] >= HEIGHT) {
-            running = false;
-        }
-        // check for right wall collision
-        if (x[0] < 0) {
-            running = false;
+        else if (y[0] >= HEIGHT) {
+            y[0] = 0;
         }
         // check for left wall collision
-        if (x[0] >= WIDTH) {
-            running = false;
+        else if (x[0] < 0) {
+            x[0] = WIDTH - UNIT_SIZE;
+        }
+        // check for right wall collision
+        else if (x[0] >= WIDTH) {
+            x[0] = 0;
         }
         
         if (!running) {
-            System.out.println("ded");
             timer.stop();
         }
         
@@ -185,8 +232,8 @@ public class SnakePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(running) {
             move();
-            checkApple();
             checkCollisions();
+            checkApple();
         }
         repaint();
 
@@ -198,27 +245,18 @@ public class SnakePanel extends JPanel implements ActionListener {
         public void keyPressed(KeyEvent e){
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_LEFT:
-                    if (direction != 'R') {
-                        direction = 'L';
-                    }
+                    commandQueue.add('L');
                      break;
                 case KeyEvent.VK_RIGHT:
-                    if (direction != 'L') {
-                        direction = 'R';
-                    }
+                    commandQueue.add('R');
                       break;
                 case KeyEvent.VK_UP:
-                    if (direction != 'D') {
-                        direction = 'U';
-                    }
+                    commandQueue.add('U');
                     break;
                 case KeyEvent.VK_DOWN:
-                    if (direction != 'U') {
-                        direction = 'D';
-                    }
+                    commandQueue.add('D');
                     break;
             }
         }
     }
-
 }
